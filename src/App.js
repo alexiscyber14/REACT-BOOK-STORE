fdcae40f87c0a8769a14ca2bd5b99bb4d4228419
alexiscyber14 <142sledgehammer14@gmail.com>
2023-03-29 11:09:55 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<CategoriesComponent />} />
+    </Routes>
+  </Router>
     </div>
   );
 }
