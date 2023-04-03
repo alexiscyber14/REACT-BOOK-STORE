4e08778426259763be3ce634370f33583fb3d981
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-03 09:08:24 +0100

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import Header from './components/Header';
-// import  { store } from './redux/store';
-// import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Header />
     <App />
-  </React.StrictMode>,
+  </Provider>,
 );
