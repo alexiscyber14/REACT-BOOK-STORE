8f3db903648d4af161544de2187cab7da97d9973
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-03 10:40:25 +0100

    display redux books

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import bookReducer from './books/bookSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    book: bookReducer,
+  },
 });
